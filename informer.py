47814c7ddf75d2e6ba4a9a8cd6fe0fc63b5f6db5
Arkoprobho Chatterjee <arko1999@gmail.com>
2020-06-08 23:15:57 +0530

    Can now open retrieved page in browser

--- informer.py
+++ informer.py
@@ -1,4 +1,5 @@
 import re
+import webbrowser
 
 from bs4 import BeautifulSoup as bs
 from urllib import request
@@ -7,12 +8,17 @@ def getPage():
 	with request.urlopen("https://en.wikipedia.org/wiki/Special:Random") as page:
 		soup = bs(page.read(), features="lxml")
 		topic = str(soup.title)[7:-20]
-		print("Found this!")
+		print("Found this!\n")
 		print(topic)
+		print("\n")
 		firstPara = str(soup.p)
 		text = re.split("<[^>]*>", firstPara)
 		print("".join(text))
-		print()
+		print("\n")
+		ch = input("Would you like to view this in your browser? Y/n\n")
+		if ch == "Y" or ch == "y":
+			webbrowser.open_new(page.geturl())
+
 
 getPage()
 while True:
