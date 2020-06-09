a4bee837167866c31365ff0dc657669627448f04
Arkoprobho Chatterjee <arko1999@gmail.com>
2020-06-09 14:39:35 +0530

    Working solution to bypass empty paras

--- informer.py
+++ informer.py
@@ -7,20 +7,23 @@ from urllib import request
 def getPage():
 	with request.urlopen("https://en.wikipedia.org/wiki/Special:Random") as page:
 		soup = bs(page.read(), features="lxml")
-		topic = str(soup.title)[7:-20]
+
+		topic = str(soup.title)[7:-20] #get rid of <title> tags and "- Wikipedia"
 		print("Found this!\n")
 		print(topic)
 		print("\n")
-		#firstPara = str(soup.p)
+
 		text = []
 		c = 0
-		while not len(text):
-			para = soup.find_all("p")
-			text = re.split("<[^>]*>", str(para[c]))
+		paras = soup.find_all("p") #get all <p> elements
+
+		while len(text) <= 3: #to avoid empty paras
+			text = re.split("<[^>]*>", str(paras[c]))
 			c += 1
 
-		print("".join(text))
+		print("".join(text)) #make it presentable
 		print("\n")
+		#TODO: remove citations from the text
 		ch = input("Would you like to view this in your browser? Y/n\n")
 		if ch == "Y" or ch == "y":
 			webbrowser.open_new(page.geturl())
